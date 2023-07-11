761eba8a93c75521988374c65d6ff04cce350c7d
BRYAN <bryangabrielberja25@gmail.com>
2023-07-11 17:39:23 +0800

    Initial commit'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, RouterProvider} from 'react-router-dom';
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'index.css'
+import HomeScreen from './screen/HomeScreen.jsx'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
